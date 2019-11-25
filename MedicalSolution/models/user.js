fcf14cb56d20e79308897f449eed0f6d563bc839
ujjwal0609 <ujjwalfriends@gmail.com>
2019-11-25 11:27:02 +0530

    first commit

---
+++ MedicalSolution/models/user.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+// User Schema
+const UserSchema = mongoose.Schema ({
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  latitude:{
+      type:Number,
+      required: true
+  },
+  longitude:{
+    type:Number,
+    required: true
+},
+ wait_time:{
+     type : Number,
+     required: true,
+     default :-1
+ }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, callback) {
+  User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback) {
+  const query = {username: username}
+  User.findOne(query, callback);
+}
+
+module.exports.addUser = function(newUser, callback) {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if(err) throw err;
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
+  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+    if(err) throw err;
+    callback(null, isMatch);
+  });
+}
+module.exports.updateTime = function(user, time ,callback){
+  User.update({_id:user._id },{ $set :{ wait_time :time}},callback)
+}
